057ae6ae8c9c257bd381646351ec649a7c3ead81
Anupong Mueangngam <86086761+stkchan@users.noreply.github.com>
2025-03-31 10:30:17 +0700

    Update checking-data-quality.sql

--- scripts/gold/checking-data-quality.sql
+++ scripts/gold/checking-data-quality.sql
@@ -35,3 +35,41 @@ GROUP BY
 	cst_id
 HAVING
 	COUNT(*) > 1;
+
+
+
+
+
+
+-- DETECTING DISTINCT values
+
+WITH x AS (
+SELECT
+	
+	DISTINCT CASE
+		WHEN ci.cst_gender IS NULL THEN ca.gen
+		ELSE ci.cst_gender
+	END AS cst_gender,
+	ca.gen
+
+
+FROM
+	silver.crm_cust_info AS ci
+LEFT JOIN
+	silver.erp_cust_az12 AS ca
+ON	ci.cst_key = ca.cid
+LEFT JOIN
+	silver.erp_loc_a101  AS la
+ON	ci.cst_key = la.cid
+
+)
+
+SELECT
+	
+	DISTINCT CASE
+		WHEN cst_gender IS NOT NULL THEN cst_gender
+		ELSE COALESCE(gen, NULL)
+	END AS x
+
+FROM
+	x
